7cc1fe4d11da0e4ea880bc76c445c3c28873e586
aryansharma <aryansharmag29@gmail.com>
2024-09-06 01:38:41 +0530

    Update Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -5,7 +5,7 @@ function Product({ pic ,category, title, price, id }) {
   return (
     <div className="flex flex-col bg-white border border-gray-200 rounded-md shadow-sm gap-y-2 p-4 max-w-xs hover:shadow-md  hover:shadow-primary-dark transition duration-200 ease-in-out">
       <div className="w-full aspect-square overflow-hidden rounded-md">
-        <img src={pic} className="w-full h-full object-cover grayscale-[40%] hover:grayscale-0 " alt="Product" />
+        <img src={pic} className="w-full h-full object-cover" alt="Product" />
       </div>
       <h2 className="text-gray-500 text-xs md:text-sm lg:text-md mt-2">{category}</h2>
       <h1 className="text-black text-xs md:text-sm lg:text-md font-semibold">{title}</h1>
